d46be678210b2bd3ebd9d8a98456b60e5c7f8570
Ignacio Bares <38444147+nachovoss@users.noreply.github.com>
2019-09-24 15:26:35 +0200

    Add files via upload

---
+++ Example.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-]
+from easyfid_master import  face_id, face_data, training_faces
+
+#face_data()
+#get data from webcam 
+training_faces()
+#training with given images
+
+face_id()
+#Start face recognition based on trained data
+            
---
+++ face_id_master.py
@@ -0,0 +1,149 @@
+#-*- coding:utf-8 -*-]
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+
+
+def face_data():
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 1920)  # video width
+    cam.set(4, 1080)  # video height
+
+    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # For each person, enter one numeric face id
+    face_id = input('\n enter user id end press <return> ==>  ')
+
+    print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+    # Initialize individual sampling face count
+    count = 0
+
+    while True:
+
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            count += 1
+
+            # Save the captured image into the datasets folder
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+
+            cv2.imshow('image', img)
+
+        k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 30:  # Take 30 face sample and stop video
+            break
+
+    # Do a bit of cleanup
+    print("\n Exiting Program and cleanup ")
+    cam.release()
+    cv2.destroyAllWindows()
+    return  face_data()
+def training_faces():
+# Path for face image database
+        path = 'dataset'
+
+        recognizer = cv2.face.LBPHFaceRecognizer_create()
+        detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
+
+        # function to get the images and label data
+        def getImagesAndLabels(path):
+
+            imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+            faceSamples=[]
+            ids = []
+
+            for imagePath in imagePaths:
+
+                PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+                img_numpy = np.array(PIL_img,'uint8')
+
+                id = int(os.path.split(imagePath)[-1].split(".")[1])
+                faces = detector.detectMultiScale(img_numpy)
+
+                for (x,y,w,h) in faces:
+                    faceSamples.append(img_numpy[y:y+h,x:x+w])
+                    ids.append(id)
+
+            return faceSamples,ids
+
+        print ("\n Training faces. It will take a few seconds. Wait ...")
+        faces,ids = getImagesAndLabels(path)
+        recognizer.train(faces, np.array(ids))
+
+        # Save the model into trainer/trainer.yml
+        recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+        # Print the numer of faces trained and end program
+        print("\n {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+
+
+def face_id():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('trainer/trainer.yml')
+    cascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath);
+
+    font = cv2.FONT_HERSHEY_TRIPLEX
+
+
+
+    nameuser1 = input('user 1 enter your name and press return(press 5 times if no more users): ')
+    nameuser2 = input('user 2 enter your name and press return: ')
+    nameuser3 = input('user 3 enter your name and press return: ')
+    nameuser4 = input('user 4 enter your name and press return: ')
+    nameuser5 = input('user 5 enter your name and press return: ')
+
+    names = ['None', nameuser1, nameuser2, nameuser3, nameuser4, nameuser5]
+
+    # Initialize and start realtime video capture
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 1280)  # set video widht
+    cam.set(4, 720)  # set video height
+
+    # Define min window size to be recognized as a face
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+
+    while True:
+
+        ret, img = cam.read()
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(int(minW), int(minH)), )
+
+        for (x, y, w, h) in faces:
+
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+
+            # Check if confidence is less them 100 ==> "0" is perfect match
+            if (confidence < 100):
+                id = names[id]
+                confidence = "  {0}%".format(round(100 - confidence))
+            else:
+                id = "unknown"
+                confidence = "  {0}%".format(round(100 - confidence))
+
+            cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+            cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+        cv2.imshow('camera', img)
+
+        k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+
+    # Do a bit of cleanup
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
