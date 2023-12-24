ada25a4bcf1759e66a05495be7db061dfc45bd68
Ignacio Bares <38444147+nachovoss@users.noreply.github.com>
2023-12-24 12:19:10 +0100

    Refactor Example.py

--- Example.py
+++ Example.py
@@ -1,11 +1,15 @@
 #-*- coding:utf-8 -*-]
-from easyfid_master import  face_id, face_data, training_faces
+from easyfid_master import FaceRecognizer
 
-face_data()
-#get data from webcam 
-training_faces()
-#training with given images
+def main():
+    face_recognizer = FaceRecognizer()
+    imgs_per_person = int(input("Enter number of pictures per person: "))
+    n_users = int(input("Enter number of users: "))
+    face_recognizer.capture_faces(imgs_per_person, n_users)
+    face_recognizer.train_faces()
+    n_names = int(input("Enter number of users for recognition: "))
+    names = ['None'] + [input(f'User {i} enter your name: ') for i in range(1, n_names + 1)]
+    face_recognizer.recognize_faces(n_names, names)
 
-face_id()
-#Start face recognition based on trained data
-            
+if __name__ == "__main__":
+    main()
