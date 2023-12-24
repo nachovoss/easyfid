76ad43886d2aa886373f952ecf97efe2eac90fd0
Ignacio Bares <38444147+nachovoss@users.noreply.github.com>
2023-12-24 12:17:01 +0100

    Refactor  easyfid_master.py

--- easyfid_master.py
+++ easyfid_master.py
@@ -76,15 +76,4 @@ class FaceRecognizer:
         self.cam.release()
         cv2.destroyAllWindows()
 
-def main():
-    face_recognizer = FaceRecognizer()
-    imgs_per_person = int(input("Enter number of pictures per person: "))
-    n_users = int(input("Enter number of users: "))
-    face_recognizer.capture_faces(imgs_per_person, n_users)
-    face_recognizer.train_faces()
-    n_names = int(input("Enter number of users for recognition: "))
-    names = ['None'] + [input(f'User {i} enter your name: ') for i in range(1, n_names + 1)]
-    face_recognizer.recognize_faces(n_names, names)
 
-if __name__ == "__main__":
-    main()
